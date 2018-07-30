d60a2367b824746bbe6c6b9b3fb1313442a766fb
Hendrik Geßner <hgessner@uni-potsdam.de>
2018-07-30 19:35:33 +0200

    Extended arxiv-lookup for MathJax syntax translation.

--- dspace-api/src/main/java/org/dspace/importer/external/arxiv/service/ArxivXpathMetadatumContributor.java
+++ dspace-api/src/main/java/org/dspace/importer/external/arxiv/service/ArxivXpathMetadatumContributor.java
@@ -8,6 +8,16 @@ import org.dspace.importer.external.metadatamapping.contributor.SimpleXpathMetad
 
 public class ArxivXpathMetadatumContributor extends SimpleXpathMetadatumContributor {
 
+	private boolean mathJaxMapped = false;
+
+	public boolean isMathJaxMapped() {
+		return mathJaxMapped;
+	}
+
+	public void setMathJaxMapped(boolean mathJaxMapped) {
+		this.mathJaxMapped = mathJaxMapped;
+	}
+
 	@Override
 	public Collection<MetadatumDTO> contributeMetadata(OMElement t) {
 		Collection<MetadatumDTO> result = super.contributeMetadata(t);
@@ -19,9 +29,39 @@ public class ArxivXpathMetadatumContributor extends SimpleXpathMetadatumContribu
 				// Also remove leading and trailing spaces
 				value = value.replaceAll("[\\s\\r\\n]+", " ").replaceAll("^\\s*|\\s*$", "");
 			}
+			if (value != null && mathJaxMapped) {
+				// arXiv uses $...$ as MathJax delimiters. From DSpace 6.3 onwards, MathJax
+				// expects \(...\) for in-line mathematics and $$...$$ for displayed
+				// mathematics. See https://jira.duraspace.org/browse/DS-3087 for details
+				int dollarIndex = -1;
+				while ((dollarIndex = findNext(value, '$', dollarIndex + 1)) != -1) {
+					int nextIndex = findNext(value, '$', dollarIndex + 1);
+					if (nextIndex != -1) {
+						value = value.substring(0, dollarIndex) + "\\(" + value.substring(dollarIndex + 1, nextIndex) + "\\)" + value.substring(nextIndex + 1);
+						dollarIndex = nextIndex + 2;
+					}
+				}
+			}
 			dto.setValue(value);
 		}
 
 		return result;
+	}
+
+	private int findNext(String value, int sign, int startFrom) {
+		int signIndex = value.indexOf('$', startFrom);
+		if (signIndex != -1) {
+			// Check for double sign
+			int nextIndex = findNext(value, sign, signIndex + 1);
+			if (nextIndex == signIndex + 1) {
+				// Skip double sign
+				return findNext(value, sign, signIndex + 2);
+			}
+
+			// Single sign confirmed
+			return signIndex;
+		} else {
+			return -1;
+		}
 	}
 }
---
+++ dspace-api/src/test/java/org/dspace/importer/external/arxiv/service/ArxivXpathMetadatumContributorTest.java
@@ -0,0 +1,59 @@
+package org.dspace.importer.external.arxiv.service;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.StringReader;
+import java.util.Collection;
+
+import javax.annotation.Resource;
+
+import org.apache.axiom.om.OMElement;
+import org.apache.axiom.om.OMXMLBuilderFactory;
+import org.apache.axiom.om.OMXMLParserWrapper;
+import org.dspace.importer.external.metadatamapping.MetadatumDTO;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:/spring/spring-dspace-addon-import-services.xml", "file:../dspace/config/spring/api/arxiv-integration.xml", "file:../dspace/config/spring/api/pubmed-integration.xml" })
+public class ArxivXpathMetadatumContributorTest {
+
+	@Resource(name = "arxivSummaryContrib")
+	private ArxivXpathMetadatumContributor sutForAbstract;
+
+	@Resource(name = "arxivDoiContrib")
+	private ArxivXpathMetadatumContributor sutForDoi;
+
+	private OMElement content;
+
+	@Before
+	public void before() {
+		String rawContent = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r\n<entry xmlns=\"http://www.w3.org/2005/Atom\">\r\n  <summary>A $10^5$ size</summary>\r\n  <arxiv:doi xmlns:arxiv=\"http://arxiv.org/schemas/atom\">doi$10^5$doi</arxiv:doi>\r\n</entry>";
+
+		OMXMLParserWrapper records = OMXMLBuilderFactory.createOMBuilder(new StringReader(rawContent));
+		this.content = records.getDocumentElement();
+	}
+
+	@Test
+	public void shouldMapMathJaxInAbstract() {
+		Collection<MetadatumDTO> result = sutForAbstract.contributeMetadata(content);
+
+		assertEquals(1, result.size());
+		for (MetadatumDTO meta : result) {
+			assertEquals("A \\(10^5\\) size", meta.getValue());
+		}
+	}
+
+	@Test
+	public void shouldNotMapMathJaxInDoi() {
+		Collection<MetadatumDTO> result = sutForDoi.contributeMetadata(content);
+
+		assertEquals(1, result.size());
+		for (MetadatumDTO meta : result) {
+			assertEquals("doi$10^5$doi", meta.getValue());
+		}
+	}
+}
